9fd4ff4e447a1d7aa30f9d1c3d85434373fe5d50
renoud <renoud@monostatic.om>
2026-03-17 14:10:23 +0100

    fix: simplify middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-export const runtime = 'nodejs'
 const isProtectedRoute = createRouteMatcher(['/mijn-omgeving(.*)'])
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
 })
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 }
